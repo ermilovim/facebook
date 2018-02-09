632aad6b80cd9592c92e9a8787ee4170e76aa98d
ermilovim <36043809+ermilovim@users.noreply.github.com>
2018-02-09 22:48:55 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,8 @@
+identificator = open("in.txt",w+)
+print("Имя файла: ", identificator.name)
+num_of_ident=int(input("Введите число идентификаторов для проверки?"))
+i=0
+print("Введите идентификаторы для проверки:")
+while i<num_of_ident:
+    i=input()
+    identificator.write
