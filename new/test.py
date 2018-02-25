52975930c48212e9cde2d77b44709d6b9aaffa80
ermilovim <ermilovim@gmail.com>
2018-02-25 13:40:14 +0300

    first step
    or not?

--- new/test.py
+++ new/test.py
@@ -2,21 +2,30 @@ import facebook
 import requests
 import sqlite3
 
-con = sqlite3.connect('users.db')
-cur = con.cursor()
-cur.execute('CREATE TABLE users (id VARCHAR(100), existence VARCHAR(15))')
-con.commit()
-cur.execute('INSERT INTO users (id, existence) VALUES("100001103449059", "NULL")')
+
+"""cur.execute('INSERT INTO users (id, existence) VALUES("100001103449059", "?")')
 con.commit()
 print (cur.lastrowid)
 
 cur.execute('SELECT * FROM users')
 print (cur.fetchall())
-con.close()
-def check_exist_user_by_id(file_witd_ids):
-    out = open('out_ids.txt', 'w')
+con.close()"""
+def check_exist_user_by_id(db_witd_ids):
+    con = sqlite3.connect('users.db')
+    cur = con.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS users (id VARCHAR(100), existence VARCHAR(15))')
+    con.commit()
+    cur.executescript("""
+     insert into users values ('100001103449059', 'null');
+     insert into users values ('000001103449059', 'null');
+     insert into users values ('zuck', 'null');
+     insert into users values ('4', 'null');
+     insert into users values ('nasksadj.masd', 'null');
+     insert into users values ('501012028', 'null');
+    """)
+    """out = open('out_ids.txt', 'w')"""
     graph = facebook.GraphAPI('1938851329475559|9N7KF0F4LokFujG_oTibHwz3YwM')
-    with open(file_witd_ids, 'r') as f:
+    with open(db_witd_ids, 'r') as f:
         ids = f.readlines()
         for id in ids:
             id = id.strip()
@@ -37,7 +46,7 @@ def check_exist_user_by_id(file_witd_ids):
                 else:
                     out.write('Exist {}\n'.format(id))
                     print(id)
-
+    con.close()
     out.close()
     return
 
