bd788244817ff26e8ff066a26d6505710e660965
ermilovim <ermilovim@gmail.com>
2018-03-02 11:04:34 +0300

    end
    /./\.\

--- new/test.py
+++ new/test.py
@@ -3,62 +3,42 @@ import requests
 import sqlite3
 
 
-"""cur.execute('INSERT INTO users (id, existence) VALUES("100001103449059", "?")')
-con.commit()
-print (cur.lastrowid)
-
-cur.execute('SELECT * FROM users')
-print (cur.fetchall())
-con.close()"""
-def check_exist_user_by_id(db_witd_ids):
+def check_exist_user_by_id():
     con = sqlite3.connect('users.db')
     cur = con.cursor()
     cur.execute('CREATE TABLE IF NOT EXISTS users (id VARCHAR(100), existence VARCHAR(15))')
     con.commit()
-    cur.executescript("""
-     insert into users values ('100001103449059', 'null');
-     insert into users values ('000001103449059', 'null');
-     insert into users values ('zuck', 'null');
-     insert into users values ('4', 'null');
-     insert into users values ('nasksadj.masd', 'null');
-     insert into users values ('501012028', 'null');
-     insert into users values ('sdfwgeop', 'null');
-    """)
+    ids = open("ids", 'r').readlines()
+    for id in ids:
+        id = id.strip()
+        cur.execute("insert into users values (?, null)", [id])
     con.commit()
-    ids1 = open('ids1.txt','w')
-    cur.execute('SELECT * FROM users')
-    while True:
-        row = cur.fetchone()
-        if row == None:
-            break
-        ids1.write(row[0])
-        ids1.write("\n")
-    out = open('out_ids.txt', 'w')
+
+    with open("ids", 'w') as f:
+        f.write('')
+
+    ids = cur.execute('SELECT id FROM users where existence is null').fetchall()
+
     graph = facebook.GraphAPI('1938851329475559|9N7KF0F4LokFujG_oTibHwz3YwM')
-    ids1.close()
-    with open('ids1.txt', 'r') as f:
-        ids = f.readlines()
-        for id in ids:
-            id = id.strip()
-            if id.isdigit():
-                try:
-                    graph.get_object(id)
-                    out.write('Exist {}\n'.format(id))
-                    #print(id)
-                except Exception as e:
-                    out.write('Not Exist {}\n'.format(id))
-                   # print(id)
-                    #print(e)
+
+    for id in ids:
+        id = id[0].strip()
+        existence = False
+        if id.isdigit():
+            try:
+                graph.get_object(id)
+                existence = True
+            except Exception as e:
+                existence = False
+        else:
+            resp = requests.get('https://www.facebook.com/{}'.format(id))
+            if 'не найдена' in resp.text:
+                existence = False
             else:
-                resp = requests.get('https://www.facebook.com/{}'.format(id))
-                if 'не найдена' in resp.text:
-                    out.write('Not Exist {}\n'.format(id))
-                   # print(id)
-                else:
-                    out.write('Exist {}\n'.format(id))
-                   # print(id)
-    con.close()
-    out.close()
+                existence = True
+        existence = 'Exists' if existence else 'Not exists'
+        cur.execute('update users set existence = "{}" where id == "{}"'.format(existence, id))
+    con.commit()
     return
 
-check_exist_user_by_id('ids')
+check_exist_user_by_id()
